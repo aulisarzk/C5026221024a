b9bfd80f0b0c42aa0213ec78e28646de66b5238d
aulisa <aulisariskiamanda@.com>
2023-11-30 15:35:48 +0700

    Pertemuan 30 Nop 2023

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,8 @@
+
+<hr />
+<br />
+<br />
+<footer>
+    <p>&copy; <a href="https://www.malasngoding.com">www.malasngoding.com</a>, 2018-2019</p>
+</footer>
+</div>
--- resources/views/tambah.blade.php
+++ resources/views/tambah.blade.php
@@ -1,28 +1,44 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>Tutorial Membuat CRUD Pada Laravel - www.malasngoding.com</title>
+    <title>Tutorial Membuat CRUD Pada Laravel - www.malasngoding.com</title>
 </head>
+
 <body>
 
-	<h2><a href="https://www.malasngoding.com">www.malasngoding.com</a></h2>
-	<h3>Data Pegawai</h3>
+    <h2><a href="https://www.malasngoding.com">www.malasngoding.com</a></h2>
+    <h3>Data Pegawai</h3>
 
-	<a href="/pegawai"> Kembali</a>
+    <a href="/pegawai"> Kembali</a>
 
-	<br/>
-	<br/>
+    <br />
+    <br />
 
-	<form action="/pegawai/store" method="post">
-		{{ csrf_field() }}
-		Nama <input type="text" name="nama"> <br/>
-		Jabatan <input type="text" name="jabatan"> <br/>
-		Umur <input type="number" name="umur"> <br/>
-		Alamat <textarea name="alamat"></textarea> <br/>
-		<input type="submit" value="Simpan Data">
-	</form>
+    <form action="/pegawai/store" method="post">
+        {{ csrf_field() }}
+
+        <div class="form-group row">
+            <label for="nama" class="col-xs-3 col-form-label mr-2">Nama</label>
+            <div class="col-xs-9">
+                <input type="text" class="form-control" id="nama" name="nama">
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <label for="jabatan" class="col-xs-3 col-form-label mr-2">Jabatan</label>
+            <div class="col-xs-9">
+                <input type="text" class="form-control" id="jabatan" name="jabatan">
+            </div>
+        </div>
+        Umur <input type="number" name="umur"> <br />
+        Alamat
+        <textarea name="alamat"></textarea> <br />
+        <input type="submit" value="Simpan Data">
+    </form>
 
 
 
 </body>
+
 </html>
